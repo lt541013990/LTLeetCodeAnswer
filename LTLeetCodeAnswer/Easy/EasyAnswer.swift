3dc551c407e64ac7dfedafa1e843498fad594b86
龙腾 <541013990@qq.com>
2018-01-09 20:12:30 +0800

    做题 9.Palindrome Number 回数      leetcode这个运行时间比较真是坑，一模一样代码多耗时50ms

--- LTLeetCodeAnswer/Easy/EasyAnswer.swift
+++ LTLeetCodeAnswer/Easy/EasyAnswer.swift
@@ -12,7 +12,7 @@ class EasyAnswer: NSObject {
     
     // MARK: - 1.Two Sum
     func execTowSumSolution() {
-        let result = twoSum1([3, 3], 6)
+        let result = isPalindrome(-1)
         print(result)
     }
     
@@ -40,6 +40,65 @@ class EasyAnswer: NSObject {
             arr.append(nums[x])
         }
         return [0, 0]
+    }
+    
+    // MARK: -9.Palindrome Number
+   
+    func isPalindrome(_ x: Int) -> Bool {
+        
+        guard x >= 10 else {
+            if x >= 0 {
+                return true
+            }
+            return false
+        }
+        
+        var numArray :Array = Array<Int>()
+        
+        var y = x
+        while y != 0 {
+            numArray.append(y % 10)
+            
+            y = y / 10
+        }
+        
+        // answer1 253 ms
+//        for m in 0 ... numArray.count / 2 - 1 {
+//            if numArray[m] != numArray[numArray.count - m - 1] {
+//                return false
+//            }
+//        }
+        
+        // answer2 284 ms
+        var resultInt = 0
+        for m in numArray {
+            resultInt = resultInt * 10 + m
+        }
+        
+        if resultInt == x {
+            return true
+        } else {
+            return false
+        }
+        
+    }
+    // answer3 200 ms
+    func isPalindrome2(_ x: Int) -> Bool {
+        
+        if x<0 {
+            return false
+        } else if x == 0 {
+            return true
+        } else {
+            var temp = x
+            var y = 0
+            while temp > 0 {
+                y = (y*10) + (temp%10)
+                temp /= 10
+            }
+            return x == y
+        }
+        
     }
     
 }
